d57d6624da9defa7468fa122f5c9d3e86eeef380
tchong6@slb.com <ngothehieusan123@gmail.com>
2022-10-14 14:19:47 +0700

    abc

--- create.sh
+++ create.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "Creating stack ..... "
-  aws cloudformation package --template-file project2-nestedStack.yml --s3-bucket super-aws-s3-udagram  > out.yml
+  aws cloudformation package --template-file nested.yml --s3-bucket super-aws-s3-udagram  > out.yml
  aws cloudformation create-stack --stack-name super-immortal-stack --template-body file://out.yml  --region=us-east-1  --capabilities "CAPABILITY_NAMED_IAM"  --profile default
 $SHELL
